759616154a6be19fb97c54e532183da3b8cc2eea
wishalNipun <wishal1974@gmail.com>
2022-04-28 11:48:49 +0530

    generateNew Id

--- Layered_Architecture/src/dao/CustomerDAOImpl.java
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -59,4 +59,15 @@ public class CustomerDAOImpl {
         pstm.setString(3, customerDTO.getId());
        return pstm.executeUpdate()>0;
     }
+    public  String generateNewId() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        ResultSet rst = connection.createStatement().executeQuery("SELECT id FROM Customer ORDER BY id DESC LIMIT 1;");
+        if (rst.next()) {
+            String id = rst.getString("id");
+            int newCustomerId = Integer.parseInt(id.replace("C00-", "")) + 1;
+            return String.format("C00-%03d", newCustomerId);
+        } else {
+            return "C00-001";
+        }
+    }
 }
