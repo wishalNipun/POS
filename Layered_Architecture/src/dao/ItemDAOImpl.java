c64519a569e3ea507e924deade8f61d3572f3557
wishalNipun <wishal1974@gmail.com>
2022-04-28 12:34:00 +0530

    add Update in itemDAOImpl

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -41,4 +41,13 @@ public class ItemDAOImpl {
 
 
     }
+    public Boolean Update(ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?");
+        pstm.setString(1, itemDTO.getDescription());
+        pstm.setBigDecimal(2, itemDTO.getUnitPrice());
+        pstm.setInt(3, itemDTO.getQtyOnHand());
+        pstm.setString(4, itemDTO.getCode());
+        return pstm.executeUpdate()>0;
+    }
 }
