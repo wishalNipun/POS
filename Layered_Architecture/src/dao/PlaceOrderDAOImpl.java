9755bdcc4aaa05623f5596eb0d66d9c60fcb1795
wishalNipun <wishal1974@gmail.com>
2022-05-01 18:46:34 +0530

    add Search Customer in PlaceOrderDAOImpl

--- Layered_Architecture/src/dao/PlaceOrderDAOImpl.java
+++ Layered_Architecture/src/dao/PlaceOrderDAOImpl.java
@@ -1,4 +1,22 @@
 package dao;
 
+import db.DBConnection;
+import model.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PlaceOrderDAOImpl {
+
+    public String search(String newValue) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Customer WHERE id=?");
+        pstm.setString(1, newValue + "");
+        ResultSet rst = pstm.executeQuery();
+        rst.next();
+        CustomerDTO customerDTO = new CustomerDTO(newValue + "", rst.getString("name"), rst.getString("address"));
+        return customerDTO.getName();
+    }
 }
