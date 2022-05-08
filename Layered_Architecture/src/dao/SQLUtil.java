80100e350d397df16de9fd11e4ec8366a9523f9e
wishalNipun <wishal1974@gmail.com>
2022-05-08 16:01:21 +0530

    refactored

--- Layered_Architecture/src/dao/SQLUtil.java
+++ Layered_Architecture/src/dao/SQLUtil.java
@@ -8,20 +8,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SQLUtil {
-    private static PreparedStatement getPreparedStatement(String sql, Object... args) throws SQLException, ClassNotFoundException {
+    private static PreparedStatement getPreparedStatement(String sql,Object... args) throws SQLException, ClassNotFoundException {
+
         Connection connection = DBConnection.getDbConnection().getConnection();
         PreparedStatement pstm = connection.prepareStatement(sql);
         for (int i = 0; i < args.length; i++) {
-            pstm.setObject(i + 1, args[i]);
+            pstm.setObject(i+1,args[i]);
         }
         return pstm;
     }
+    public static boolean executeUpdate(String sql,Object... args) throws SQLException, ClassNotFoundException {
+        return getPreparedStatement(sql, args).executeUpdate()>0;
 
-    public static boolean executeUpdate(String sql, Object... args) throws SQLException, ClassNotFoundException {
-        return getPreparedStatement(sql, args).executeUpdate() > 0;
     }
-
-    public static ResultSet executeQuery(String sql, Object... args) throws SQLException, ClassNotFoundException {
-        return getPreparedStatement(sql, args).executeQuery();
+    public static ResultSet executeQuery(String sql,Object... args) throws SQLException, ClassNotFoundException {
+        return getPreparedStatement(sql,args).executeQuery();
     }
 }
